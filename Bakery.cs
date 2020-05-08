713ae0a12e3eca97a7cc0d033f2eb8b849a986a9
Tyler Bates <Fondlytyler@gmail.com>
2020-05-08 12:01:33 -0700

    refactor code to remove bugged dictionary.

--- Bakery.cs
+++ Bakery.cs
@@ -8,14 +8,14 @@ namespace Bakery.Menu
     public int BagPrice { get; set; }
     public int CrosPrice { get; set; }
     public int BreadTotal { get; set; }
-    private Dictionary<int, Action> BreadMenu { get; set; }
+    // private Dictionary<int, Action> BreadMenu { get; set; }
 
     public Bread()
     {
-      // BagPrice = bagPrice;
-      // CrosPrice = crosPrice;
+      BagPrice = 0;
+      CrosPrice = 0;
       BreadTotal = 0;
-      BreadMenu = new Dictionary<int, Action>() { {1, () => BaguettePrice()}, {2, () => CroissantPrice()} };
+      // BreadMenu = new Dictionary<int, Action>() { {1, () => BaguettePrice()}, {2, () => CroissantPrice()} };
     }
 
 
@@ -25,26 +25,28 @@ namespace Bakery.Menu
       Console.WriteLine("   'B' for Bread |  'P' for Pastries");
       string menuType = (Console.ReadLine().ToUpper());
 
-      if( menuType == "P")
+      if( menuType == "B")
       {
-        PMenu();
+        BMenu();
       }
       else
       {
-        BMenu();
+        Environment.Exit(0);
       }
     }
 
     public static void BMenu()
     {
       Console.WriteLine(@"
-  ___                 _   __  __                _ 
- | _ )_ _ ___ __ _ __| | |  \/  |___ _ _ _  _  (_)
- | _ \ '_/ -_) _` / _` | | |\/| / -_) ' \ || |  _ 
- |___/_| \___\__,_\__,_| |_|  |_\___|_||_\_,_| (_)
+██████╗ ██████╗ ███████╗ █████╗ ██████╗ 
+██╔══██╗██╔══██╗██╔════╝██╔══██╗██╔══██╗
+██████╔╝██████╔╝█████╗  ███████║██║  ██║
+██╔══██╗██╔══██╗██╔══╝  ██╔══██║██║  ██║
+██████╔╝██║  ██║███████╗██║  ██║██████╔╝
+╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═════╝
                                                   
          Today's Special:
-         Buy 2 get of FREE");
+         Buy 2 get 1 FREE");
       
       Console.WriteLine(@"1. Baguette($5)      2. Croissant($5)
       3. Sourdough Roll($5) 4. Rosemary Potato Roll($5)
@@ -52,60 +54,62 @@ namespace Bakery.Menu
       Please input a number between 1 - 4 then hit ENTER:");
 
       int item = int.Parse(Console.ReadLine());
-      BreadMenu[item].invoke();
-    }
-
-    private void BaguettePrice()
-    {
-      Console.WriteLine("How many Baguettes would you like?");
+      // BreadMenu[item].invoke();
+      if( item == 1)
+      {
+        Console.WriteLine("How many Baguettes would you like?");
       
-      int bagAmount = int.Parse(Console.ReadLine());
-      int discount = bagAmount / 3;
-      BagPrice = (bagAmount - discount)* 5;
-      Console.WriteLine("You've ordered $" + BagPrice + " worth of Baguettes.");
+        int bagAmount = int.Parse(Console.ReadLine());
+        int discount = bagAmount / 3;
+        int BagPrice = (bagAmount - discount)* 5;
+        Console.WriteLine("You've ordered $" + BagPrice + " worth of Baguettes.");
 
-      Console.WriteLine("Would you like to checkout? Y/N");
-      string check = (Console.ReadLine().ToUpper());
+        Console.WriteLine("Would you like to checkout? Y/N");
+        string check = (Console.ReadLine().ToUpper());
        
-      if(check == "Y")
+        if(check == "N")
+        {
+          StartMenu();
+        }
+        else
+        {
+          Environment.Exit(0);
+        }
+      }
+      else if (item == 2)
       {
-        Checkout();
+         Console.WriteLine("How many Croissants would you like?");
+        int crosAmount = int.Parse(Console.ReadLine());
+        int discount = crosAmount / 3;
+        int CrosPrice = (crosAmount - discount)* 5;
+        Console.WriteLine("You've ordered $" + CrosPrice + " worth of Croissants.");
+
+        Console.WriteLine("Would you like to checkout? Y/N");
+        string check = (Console.ReadLine().ToUpper());
+
+        if(check == "N")
+        {
+          StartMenu();
+        }
+        else
+        {
+          Environment.Exit(0);
+        }
       }
       else
       {
-        StartMenu();
+        Environment.Exit(0);
       }
-    }
-
-    private void CroissantPrice()
-    {
-      Console.WriteLine("How many Croissants would you like?");
-      int crosAmount = int.Parse(Console.ReadLine());
-      int discount = crosAmount / 3;
-      CrosPrice = (crosAmount - discount)* 5;
-      Console.WritLine("You've ordered $" + CrosPrice + " worth of Croissants.");
-
-      Console.WriteLine("Would you like to checkout? Y/N");
-      string check = (Console.ReadLine().ToUpper());
-
-      if(check == "Y")
-      {
-        CheckOut();
-      }
-      else
-      {
-        StartMenu();
-      }
-    }    
+    }   
   }
 
 
-  class Pastry
-  {
-    public string PastryType { get; set; }
-    public int PastryAmount { get; set; }
-    public int PastryPrice { get; set; }
-    private Dictionary<int, Action> PastryMenu { get; set; }
-  }
+  // class Pastry
+  // {
+  //   public string PastryType { get; set; }
+  //   public int PastryAmount { get; set; }
+  //   public int PastryPrice { get; set; }
+  //   private Dictionary<int, Action> PastryMenu { get; set; }
+  // }
 
 }
